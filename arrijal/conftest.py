613fe1ce1243645f31f6ff9491c0a78c066d5aa0
mahidul-islam <mizihan84@gmail.com>
2020-04-21 08:04:12 +0600

    bismillah!

---
+++ arrijal/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from arrijal.users.models import User
+from arrijal.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
