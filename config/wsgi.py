8b24be20be22daf304d13c85ad687df3bdf93734
mahidul-islam <mizihan84@gmail.com>
2020-04-24 13:31:35 +0600

    checking if work

--- config/wsgi.py
+++ config/wsgi.py
@@ -41,11 +41,11 @@ sys.path.append(str(app_path / "arrijal"))
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ['DJANGO_SECRET_KEY'] = '23948*(&@#$*&(@#*$&(@#*$&fjlsajflsdfj'
-os.environ['DJANGO_ALLOWED_HOSTS'] = 'arrijal.company www.arrijal.company'
-os.environ['DJANGO_ADMIN_URL'] = 'admin/'
-os.environ['DATABASE_URL'] = 'postgres://zihan:1234@127.0.0.1:5432/localcookiedb'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+# os.environ['DJANGO_SECRET_KEY'] = '23948*(&@#$*&(@#*$&(@#*$&fjlsajflsdfj'
+# os.environ['DJANGO_ALLOWED_HOSTS'] = 'arrijal.company www.arrijal.company'
+# os.environ['DJANGO_ADMIN_URL'] = 'admin/'
+# os.environ['DATABASE_URL'] = 'postgres://zihan:1234@127.0.0.1:5432/localcookiedb'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
