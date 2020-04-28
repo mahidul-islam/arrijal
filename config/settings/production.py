b90171d87b055691aacac289d6acd0ab38130bcc
mahidul-islam <mizihan84@gmail.com>
2020-04-28 12:31:27 +0600

    removed password to .env

--- config/settings/production.py
+++ config/settings/production.py
@@ -106,7 +106,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'mizihan84@gmail.com'
-EMAIL_HOST_PASSWORD = 'zihancse14'
+EMAIL_HOST_PASSWORD = env("GMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
