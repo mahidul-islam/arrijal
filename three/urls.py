215573c8159bfe95588f8f5d11dcd4f96b38042e
mahidul-islam <mizihan84@gmail.com>
2020-05-02 21:57:07 +0600

    debug experiment not loading

--- three/urls.py
+++ three/urls.py
@@ -6,7 +6,7 @@ app_name = "three"
 urlpatterns = [
     path("", views.three, name="three"),
     path("hydrogen", views.hydrogen, name="hydrogen"),
-    path("experiment", views.hydrogen, name="experiment"),
-    path("carbonDiOxide", views.hydrogen, name="carbonDiOxide"),
-    path("water", views.hydrogen, name="water"),
+    path("experiment", views.experiment, name="experiment"),
+    path("carbonDiOxide", views.carbonDiOxide, name="carbonDiOxide"),
+    path("water", views.water, name="water"),
 ]
