34ee7a1b418c5fe920656399424aa1640c8dd925
mahidul-islam <mizihan84@gmail.com>
2020-05-04 13:42:01 +0600

    added model form admin in newsletter

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class NewsletterUser(models.Model):
+    email = models.EmailField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.email
