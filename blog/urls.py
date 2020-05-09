ca1358980c59b2442443fddab7a4009cfaaf14a5
samiulhoque67 <samiulhoque67@gmail.com>
2020-05-09 21:46:57 +0600

    Read More is updated,single image on blog_index_page has been corrected,blog.html is copied to blog_index_page.html,category.html is also moved to blog folder under template directory

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = "blog"
 urlpatterns = [
     path("", views.blogpage, name="blog"),
     path('category-dao/<category_id>/', views.category_dao, name='category-dao'),
+    path('single_post/<post_id>/', views.singlepage, name='single_post'),
     #path("delete/<int:id>/", views.delete, name="delete"),
     #path("edit/<int:id>/", views.edit, name="edit"),
     #path("readmore/<int:id>/", views.readmore, name="readmore"),
--- blog/views.py
+++ blog/views.py
@@ -16,17 +16,21 @@ def blogpage(request):
     category=BlogCategory.objects.all()
     tagIndex=BlogTagIndexPage.objects.all()
 
-    return render(request,'blog.html',{'posts':posts,'tags':tagIndex,'categori':category})
+    return render(request,'blog/blog_index_page.html',{'posts':posts,'tags':tagIndex,'categori':category})
 
 def category_dao(request,category_id):
     category=BlogCategory.objects.get(pk=category_id)
     categories=BlogCategory.objects.all()
     posts=BlogPage.objects.all()
-    return render(request,'category.html',{'single_category':category,'categori':categories,'posts':posts})
+    return render(request,'blog/category.html',{'single_category':category,'categori':categories,'posts':posts})
 
-def blogtag(request):
-        posts=BlogPage.objects.all()
-        category=BlogCategory.objects.all()
-        tagIndex=BlogTagIndexPage.objects.all()
 
-        return render(request,'blog.html',{'posts':posts,'tags':tagIndex,'categori':category})
+def singlepage(request,post_id):
+    post=BlogPage.objects.get(pk=post_id)
+    categories=BlogCategory.objects.all()
+    posts=BlogPage.objects.all()
+    return render(request,'blog/blog_page.html',{'page':post,'categori':categories,'posts':posts})
+
+def blog_tag_index_page(request):
+    categories=BlogCategory.objects.all()
+    return render(request,'blog/blog_tag_index_page.html',{'categori':categories})
