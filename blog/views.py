220022b905ea18ebfac4c536575ea63e3098a5f4
samiulhoque67 <samiulhoque67@gmail.com>
2020-05-10 11:05:28 +0600

    single-page url has been updated ,abous us deleted from navbar,author image added

--- blog/views.py
+++ blog/views.py
@@ -4,34 +4,14 @@ from django.shortcuts import render
 from .models import BlogPage
 from .models import BlogCategory
 from .models import BlogTagIndexPage
-def blogpage2(request):
-
-    category=BlogCategory.objects.all()
-    posts=BlogPage.objects.all()
-    print(category)
-    return render(request,'blogbase.html',{'categori':category,'posts':posts})
 
 def blogpage(request):
     posts=BlogPage.objects.all()
     category=BlogCategory.objects.all()
-    tagIndex=BlogTagIndexPage.objects.all()
-
-    return render(request,'blog/blog_index_page.html',{'posts':posts,'tags':tagIndex,'categori':category})
+    return render(request,'blog/blog_index_page.html',{'posts':posts,'categori':category})
 
 def category_dao(request,category_id):
     category=BlogCategory.objects.get(pk=category_id)
     categories=BlogCategory.objects.all()
     posts=BlogPage.objects.all()
     return render(request,'blog/category.html',{'single_category':category,'categori':categories,'posts':posts})
-
-
-def singlepage(request,post_id):
-    post=BlogPage.objects.get(pk=post_id)
-    categories=BlogCategory.objects.all()
-    posts=BlogPage.objects.all()
-    return render(request,'blog/blog_page.html',{'page':post,'categori':categories,'posts':posts})
-
-def nothing(request):
-    categories=BlogCategory.objects.all()
-    posts=BlogPage.objects.all()
-    return render(request,'blog/nothing.html',{'categori':categories,'posts':posts})
