6661252f866528d3ea1d2174846efeaf63692390
samiulhoque67 <samiulhoque67@gmail.com>
2020-05-10 16:42:49 +0600

    pagination added as like as review app

--- blog/models.py
+++ blog/models.py
@@ -14,17 +14,43 @@ from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 from wagtail.core.models import Page, Orderable
 from wagtail.snippets.models import register_snippet
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from wagtail.core.models import Page
 
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+    template = "blog/blog_index_page.html"
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        # Get all posts
+        all_posts = BlogPage.objects.live().public().order_by('-first_published_at')
+        # Paginate all posts by 2 per page
+        paginator = Paginator(all_posts, 2)
+        # Try to get the ?page=x value
+        page = request.GET.get("page")
+        try:
+            # If the page exists and the ?page=x is an int
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If the ?page=x is not an int; show the first page
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            posts = paginator.page(paginator.num_pages)
+
+        # "posts" will have child pages; you'll need to use .specific in the template
+        # in order to access child properties, such as youtube_video_id and subtitle
+        context["posts"] = posts
         return context
+
+
 class BlogPageTag(TaggedItemBase):
     content_object = ParentalKey(
         'BlogPage',
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,24 @@ from django.shortcuts import render
 from .models import BlogPage
 from .models import BlogCategory
 from .models import BlogTagIndexPage
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 
 def blogpage(request):
     posts=BlogPage.objects.all()
     category=BlogCategory.objects.all()
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    paginator = Paginator(posts, 2)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,'blog/blog_index_page.html',{'posts':posts,'categori':category})
 
 def category_dao(request,category_id):
