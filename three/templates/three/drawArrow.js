521723da67b6580e613a125ee16d640152a71e0a
mahidul-islam <mizihan84@gmail.com>
2020-05-15 11:00:20 +0600

    Changed arrow added links, mostly

--- three/templates/three/drawArrow.js
+++ three/templates/three/drawArrow.js
@@ -1,12 +1,14 @@
 function drawArrow(mol1, mol2) {
-  const to = new THREE.Vector3();
-  mol2.getWorldPosition(to);
-  const from = new THREE.Vector3();
-  mol1.getWorldPosition(from);
-  const direction = to.clone().sub(from);
+  const to = new THREE.Vector3()
+  mol2.getWorldPosition(to)
+  const from = new THREE.Vector3()
+  mol1.getWorldPosition(from)
+  const direction = to.clone().sub(from)
   // const radious =
-  const length = direction.length();
+  const length = direction.length() - 200
+  const arrowHeadL = 200
+  const arrowHeadW = 80
   const hex = 0x00AA00;
-  const arrow = new THREE.ArrowHelper( direction.normalize(), from, length, hex );
-  scene.add( arrow );
+  const arrow = new THREE.ArrowHelper( direction.normalize(), from, length, hex, arrowHeadL, arrowHeadW );
+  scene.add( arrow )
 }
