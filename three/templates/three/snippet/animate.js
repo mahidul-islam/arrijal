6bce3993ebb63aba60d53d87d76772c5a4c97e50
mahidul-islam <mizihan84@gmail.com>
2020-06-08 21:14:04 +0600

    Added font to molicule name, Added header to aromatic chart, added button to work on and many small change

--- three/templates/three/snippet/animate.js
+++ three/templates/three/snippet/animate.js
@@ -16,16 +16,16 @@ function animate() {
 
   if(rotatate == true){
     objects.forEach((obj) => {
-      obj.rotation.x = time;
-      obj.rotation.y = time * 0.7;
+      obj.rotation.x = time * 2;
+      obj.rotation.y = time * 0.7 * 2;
     });
     objects2.forEach((obj) => {
-      obj.rotation.y = time;
-      obj.rotation.z = time * 0.7;
+      obj.rotation.y = time * 2;
+      obj.rotation.z = time * 0.7 * 2;
     });
     objects3.forEach((obj) => {
-      obj.rotation.x = time;
-      obj.rotation.z = time * 0.7;
+      obj.rotation.x = time * 2;
+      obj.rotation.z = time * 0.7 * 2;
     });
   }
 
