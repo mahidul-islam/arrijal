f2790826d2e35ba0b096d6fca0538d8aed8246f0
samiulhoque67 <samiulhoque67@gmail.com>
2020-06-12 21:02:29 +0600

    responsive svg added

--- edumap/urls.py
+++ edumap/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.edumap, name="edumap"),
     path("firstMap", views.firstMap, name="firstMap"),
     path("bangladesh1", views.bangladesh, name="bangladesh1"),
+    path("bangladesh0", views.bangladesh0, name="bangladesh0"),
+
 ]
