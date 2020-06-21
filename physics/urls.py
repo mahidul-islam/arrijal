0119a9297b6a90d7a2d9a8fa11e2404a2fa0ee57
mahidul-islam <mizihan84@gmail.com>
2020-06-21 10:50:50 +0600

    Added a basic 3d game

--- physics/urls.py
+++ physics/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.physics, name="physics"),
     path("firstGame/", views.firstGame, name="firstGame"),
     path("mod2d/", views.mod2d, name="mod2d"),
+    path("threed/", views.threed, name="threed"),
 ]
